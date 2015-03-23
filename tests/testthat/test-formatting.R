2e27fbc5b1ff2ecc8240f8c4cf2c985edc8ee0cf
LCHansson <love.hansson@gmail.com>
2015-03-23 22:05:12 +0100

    Mock test (to be added as soon as we have working example files)

---
+++ tests/testthat/test-formatting.R
@@ -0,0 +1,9 @@
+library(format.tables)
+
+context("Table output format")
+
+test_that("Tables generate properly to LaTeX", {
+  # test_table <- read.csv("inst/examples/test_table.csv")
+  # expect_equal_to_reference(format.tables(test_table), "test_table.rds")
+  expect_equal(1, 1)
+})
